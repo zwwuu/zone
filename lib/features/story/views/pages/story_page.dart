f7a5f09a6518ddcca514bef539cadd64500e1eb2
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-02-26 18:35:33 -0500

    add app bar for navigation

--- lib/features/story/views/pages/story_page.dart
+++ lib/features/story/views/pages/story_page.dart
@@ -16,52 +16,46 @@ class StoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: GestureDetector(
-        onHorizontalDragEnd: (details) {
-          if (details.primaryVelocity! > 8) {
-            Navigator.pop(context);
-          }
-        },
-        child: Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                StoryInfo(story: story),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Column(
-                    children: [
-                      if (story.deleted) ...[
-                        const Text('[deleted]',
-                            style: TextStyle(color: Colors.grey)),
-                      ] else if (story.text.isNotEmpty)
-                        HtmlContent(
-                            html: story.text,
-                            textStyle: Theme.of(context).textTheme.bodyLarge),
-                      if (story.type == ItemType.poll) const StoryPoll(),
-                      StoryActions(story: story),
-                    ],
-                  ),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              StoryInfo(story: story),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Column(
+                  children: [
+                    if (story.deleted) ...[
+                      const Text('[deleted]',
+                          style: TextStyle(color: Colors.grey)),
+                    ] else if (story.text.isNotEmpty)
+                      HtmlContent(
+                          html: story.text,
+                          textStyle: Theme.of(context).textTheme.bodyLarge),
+                    if (story.type == ItemType.poll) const StoryPoll(),
+                    StoryActions(story: story),
+                  ],
                 ),
-                CommentList(commentIds: story.kids, by: story.by)
-              ],
-            ),
+              ),
+              CommentList(commentIds: story.kids, by: story.by)
+            ],
           ),
-          floatingActionButton: FloatingActionButton(
-            tooltip: 'View story',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => StoryWebview(
-                      url: story.url ??
-                          'https://news.ycombinator.com/item?id=${story.id}'),
-                ),
-              );
-            },
-            child: const Icon(Icons.newspaper),
-          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'View story',
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => StoryWebview(
+                    url: story.url ??
+                        'https://news.ycombinator.com/item?id=${story.id}'),
+              ),
+            );
+          },
+          child: const Icon(Icons.newspaper),
         ),
       ),
     );
